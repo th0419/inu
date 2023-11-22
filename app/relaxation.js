c82fc9ef3f121efadd2dc4e25a4efb9f929de600
ShokiSugimoto <shokisugimoto26@gmail.com>
2023-11-22 11:25:16 +0900

    p

--- app/relaxation.js
+++ app/relaxation.js
@@ -272,7 +272,7 @@ const styles = StyleSheet.create({
   selectedTag: {
     borderStyle: 'solid',
     borderColor: '#FFFFFF',
-    borderWidth: 1,
+    borderWidth: 1
   },
   text: {
     fontSize: 28,
